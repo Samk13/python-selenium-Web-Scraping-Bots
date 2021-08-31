2beecdc7dbe8858a1a1b5344a2c0cfe2310e737c
Samk13 <samarbid13@gmail.com>
2021-09-01 00:30:23 +0200

    another example for selecting elements

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 path = r"C:/SeleniumDrivers"
-testingWebpage = "https://www.seleniumeasy.com/test/jquery-download-progress-bar-demo.html"
+testingWebpage = "https://www.seleniumeasy.com/test/basic-first-form-demo.html"
 
 # you should download chromedriver and put it in the same directory as this file
 # https://chromedriver.storage.googleapis.com/index.html?path=92.0.4515.43/
@@ -14,13 +12,24 @@ driver = webdriver.Chrome()
 
 # open the website
 driver.get(testingWebpage)
-driver.implicitly_wait(10)
+driver.implicitly_wait(5)
 
-# find the download button
-downloadBtn = driver.find_element_by_id("downloadButton")
-downloadBtn.click()
+# close the ads
+try:
+    no_button = driver.find_element_by_class_name("at-cm-no-button")
+    no_button.click()
+except:
+    print("No ads found, skipping ...")
 
-#  get progress element after it is completed
-WebDriverWait(driver, 30).until(EC.text_to_be_present_in_element(
-    (By.CLASS_NAME, "progress-label"), "Complete!")
-)
+# find sum buttons
+sum1 = driver.find_element_by_id("sum1")
+sum2 = driver.find_element_by_id("sum2")
+
+# enter the numbers using the keys
+sum1.send_keys(Keys.NUMPAD1 + Keys.NUMPAD4)
+sum2.send_keys(Keys.NUMPAD1 + Keys.NUMPAD4)
+
+# get the submit numbers button
+
+btn = driver.find_element_by_css_selector('button[onClick="return total()"]')
+btn.click()
